c7c17816877ebeaee00c428cfc3d3f4f69dd4abf
codethi <docencia.thiago@gmail.com>
2022-04-02 16:15:32 -0300

    create tweet complete

--- src/tweets/Tweet.js
+++ src/tweets/Tweet.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const TweetSchema = new monsgoose.Schema({
+const TweetSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
