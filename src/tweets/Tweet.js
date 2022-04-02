b8417de8653981e041b451d55a91e6b07bce2676
codethi <docencia.thiago@gmail.com>
2022-04-02 15:50:11 -0300

    add model tweet

---
+++ src/tweets/Tweet.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const TweetSchema = new monsgoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  likes: {
+      type: Array,
+      required: true,
+  },
+  comment: {
+    type: Array,
+    required: true,
+  },
+  retweet: {
+    type: Array,
+    required: true,
+  },
+});
+
+const Tweet = mongoose.model("Tweet", TweetSchema);
+
+module.exports = Tweet;
