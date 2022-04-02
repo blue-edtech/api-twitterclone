d042a4f852d8f907e5d08b5f04adc2f44be07c5e
codethi <docencia.thiago@gmail.com>
2022-04-02 11:34:59 -0300

    add auth middleware

--- src/users/users.service.js
+++ src/users/users.service.js
@@ -6,8 +6,11 @@ const createUserSevice = (body) => User.create(body);
 
 const findAllUserService = () => User.find();
 
+const findByIdUserService = (idUser) => User.findById(idUser);
+
 module.exports = {
   findByEmailUserService,
   createUserSevice,
   findAllUserService,
+  findByIdUserService,
 };
