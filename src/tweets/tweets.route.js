86b4a661f6df42c2b79d3179c3bfee41fa46e719
codethi <docencia.thiago@gmail.com>
2022-04-04 20:22:33 -0300

    add swagger

--- src/tweets/tweets.route.js
+++ src/tweets/tweets.route.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const tweetController = require("./tweets.controller");
 const authMiddleware = require("../auth/auth.middleware");
 
-router.post("/", authMiddleware, tweetController.createTweetController);
+router.post("/create", authMiddleware, tweetController.createTweetController);
 router.get("/", authMiddleware, tweetController.findAllTweetsController);
 router.get("/search", authMiddleware, tweetController.searchTweetController);
 router.patch("/:id/like", authMiddleware, tweetController.likeTweetController);
